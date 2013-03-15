38d8b6ec7a90ec52f586be3fb37736254d53e1bd
Dublekfx <dublekfx@gmail.com>
2013-03-15 03:12:56 -0400

    Started HeadHunter Project

---
+++ src/com/github/dublekfx/HeadHunter/HeadHunter.java
@@ -0,0 +1,25 @@
+package com.github.dublekfx.HeadHunter;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
+public final class HeadHunter extends JavaPlugin implements Listener	{
+	
+	@Override
+	public void onEnable()	{
+		
+	}
+	
+	@Override
+	public void onDisable()	{
+		
+	}	
+	
+	@EventHandler
+	public void onPlayerDeath (EntityDamageByEntityEvent event)	{
+	
+	}
+
+}
