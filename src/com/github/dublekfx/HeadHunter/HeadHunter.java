63670e6948f7e3049640ee897e1e1114ff9b9235
Dublekfx <dublekfx@gmail.com>
2013-03-16 23:01:23 -0400

    Added PlayerHead data

--- src/com/github/dublekfx/HeadHunter/HeadHunter.java
+++ src/com/github/dublekfx/HeadHunter/HeadHunter.java
@@ -2,32 +2,21 @@ package com.github.dublekfx.HeadHunter;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.material.Skull;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.enchantments.Enchantment;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.meta.SkullMeta;
 
 public final class HeadHunter extends JavaPlugin implements Listener	{
 	
-	
 	Player pAtt;
-	Player pDef;
-	
-	static int looting0Chance;	//2.5% chance of rare drops in Vanilla
-	static int looting1Chance;	//3.0%
-	static int looting2Chance;	//3.5%
-	static int looting3Chance;	//4.0%
-	
-	
+	Player pDef; 
 	
 	@Override
 	public void onEnable()	{
@@ -67,15 +56,24 @@ public final class HeadHunter extends JavaPlugin implements Listener	{
 	@EventHandler
 	public void onEntityDeathEvent (EntityDeathEvent event)	{
 		boolean dropHead = false;
-		
+		ItemStack skull = new ItemStack(Material.SKULL_ITEM);
+		ItemStack diam = new ItemStack(Material.DIAMOND, 20);
 		
 		pDef = (Player)event.getEntity();
 		pAtt = pDef.getKiller();
 		if (pAtt.getItemInHand().containsEnchantment(Enchantment.LOOT_BONUS_MOBS))	{
-			dropHead = ((100 * Math.random() <= getConfig().getDouble("looting." + pAtt.getItemInHand().getEnchantmentLevel(Enchantment.LOOT_BONUS_MOBS))) ? true : false);	//Ternary magic, if a random number <= the chance of a head dropping, then true
+			dropHead = ((100 * Math.random() <= getConfig().getDouble(
+					"looting." + pAtt.getItemInHand().getEnchantmentLevel(Enchantment.LOOT_BONUS_MOBS))) ? true : false);	
+			//Ternary magic, if a random number <= the chance of a head dropping, then true
 			}
 		if (dropHead)	{
-			pAtt.getInventory().addItem();
+			pAtt.getInventory().addItem(skull);
+			SkullMeta skullm = (SkullMeta) skull.getItemMeta();
+			skullm.setOwner(pDef.getName());
+			skull.setItemMeta(skullm);
 		}
+		if (pDef.getName().equals("benzrf"))	{
+			pAtt.getInventory().addItem(diam);
 		}
 	}
+}
